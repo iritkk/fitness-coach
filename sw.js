2dea76aa6b5456a4bfe0625a63a2e118a668f281
iritkk <fabian.tiringer@gmail.com>
2026-02-03 12:53:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-coach-v4';
+const CACHE_NAME = 'fitness-coach-v5';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
